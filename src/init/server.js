792bd889c8d59f0495e8f1b5d09b53bf61d6b9c4
Andrii Mulyk <amulyk@lectrum.io>
2020-01-27 20:08:24 +0200

    add files

--- src/init/server.js
+++ src/init/server.js
@@ -2,8 +2,8 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-// Schema
-import schema from './schema.graphql';
+// Schema Types
+import schema from './types.graphql';
 
 // Resolvers
 import { resolvers } from './resolvers';
